f91f33568c810753aead6f029d12071d0e446301
sanlouise <sandra.adamshallie@gmail.com>
2016-07-25 22:03:00 +0200

    Add Comment Component

--- public/scripts/custom.js
+++ public/scripts/custom.js
@@ -12,24 +12,39 @@ var CommentBox = React.createClass({displayName: 'CommentBox',
 });
 
 var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+var Comment = React.createClass({
 	render: function() {
 		return (
-			React.createElement('div', {className: "commentList"},
-			"Hello, I am a comment list!"
-			)
-		);
+			<div className="comment">
+				<h2 className="commentAuthor">
+					{this.props.author}
+				</h2>
+				{this.props.children}
+			</div>
+		)
 	}
 })
 
-var CommentForm = React.createClass({
-	render: function() {
-		return (
-			React.createElement('div', {className: "commentForm"},
-				"Hello world, I am a comment form!"
-			)
-		);
-	}
-});
+
 
 ReactDOM.render(
 	React.createElement(CommentBox, null),
